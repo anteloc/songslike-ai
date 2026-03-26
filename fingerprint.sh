2c4b95f2b2a722a445ff450a7ca9e2e9676dd303
anteloc <6914284+anteloc@users.noreply.github.com>
2026-03-26 01:33:19 +0100

    fingerprint.sh updated, now only works with directories

--- fingerprint.sh
+++ fingerprint.sh
@@ -2,53 +2,30 @@
 
 script_dir="$(dirname "$(realpath "$0")")"
 
-# Create fingerprints for every given audio file in the given directory (non-recursively), or single file if a file is given
+source_dir="$1"
+output_dir="$2"
 
-function fingerprint_file() {
-    local audio_file="$1"
-    local base_audio_name="$(basename "$audio_file")"
-    local lyrics_file="${audio_file%.*}.lrc"
-    local fingerprint_stem="${audio_file%.*}"
-    echo "Processing: '$base_audio_name'..."
-    
-    echo "Downloading lyrics for '$base_audio_name'..."
-    $script_dir/download_lyrics.sh "$audio_file" "$lyrics_file"
-
-    echo "Generating fingerprint for '$base_audio_name', stem: '$fingerprint_stem'..."
-    python $script_dir/singing_srt_audio_merge.py "$audio_file" "$lyrics_file" "$fingerprint_stem" --no-ts
-}
-
-target="$1"
-
-if [ -z "$target" ]; then
-    echo "Usage: $0 <audio_file_or_directory>"
+if [ -z "$source_dir" ] || [ -z "$output_dir" ]; then
+    echo "Usage: $0 <audio_files_with_lyrics_dir> <output_directory>"
+    echo "Processes all audio files in the given source directory (non-recursively) and writes the .mus.txt and .lrc.txt outputs to the specified output directory."
+    echo "Example: $0 /path/to/audio_files_with_lyrics /path/to/output_dir"
+    echo "Note: The source directory should contain pairs of audio file and lyrics with the same stem, e.g. 'song.mp3' and 'song.lrc'."
     exit 1
 fi
 
-target="$(realpath "$target")"
-
-# verify that target exists
-if [ ! -e "$target" ]; then
-    echo "Error: Target '$target' not found."
+# verify that source_dir exists
+if [ ! -d "$source_dir" ]; then
+    echo "Error: Source directory '$source_dir' not found."
     exit 1
 fi
 
-# If target is a file, process it and early exit
-if [ -f "$target" ]; then
-    fingerprint_file "$target"
-    exit 0
-fi
+source_dir="$(realpath "$source_dir")"
 
-# If target is a directory, process all audio files in it (non-recursively)
-audio_files=$(find "$target" -maxdepth 1 -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" \))
-counter=1
+# If source_dir is a directory, process all audio files in it (non-recursively)
+audio_files=$(find "$source_dir" -maxdepth 1 -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" \))
 total=$(echo "$audio_files" | wc -l)
 
-echo "Found $total audio files in directory '$target'."
+echo "Found $total audio files in directory '$source_dir'."
 
-echo "$audio_files" | while read -r audio_file; do
-    echo "Processing file $counter of $total: '$audio_file'"
-    fingerprint_file "$audio_file"
-    counter=$((counter + 1))
-done
+python $script_dir/audio_lyrics_process.py "$source_dir" "$output_dir"
 
